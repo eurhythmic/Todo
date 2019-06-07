b2f7aaa33ce369ecf1a30039874f5b790691b59b
user <me@mail.com>
2019-06-07 15:59:05 -0700

    Add data persistence using UserDefaults.

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -12,9 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["item1", "item2", "item3"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,6 +60,8 @@ class TodoListViewController: UITableViewController {
             //what happens when button is clicked
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
